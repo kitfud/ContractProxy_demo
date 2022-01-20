c2d53675506ee98b25e4b2049a251eabd7512dfb
Kit Fuderich <kitfuderich@gmail.com>
2022-01-19 17:08:00 -0800

    unit and integration test added to test transparent proxy deployment

--- scripts/deploy_and_upgrade.py
+++ scripts/deploy_and_upgrade.py
@@ -12,7 +12,7 @@ from brownie import (
 def main():
     account = get_account()
     print(f"deploying to {network.show_active()}")
-    box = Box.deploy({"from": account})
+    box = Box.deploy({"from": account}, publish_source=True)
     print(box.retrieve())
 
     proxy_admin = ProxyAdmin.deploy({"from": account})
@@ -20,6 +20,7 @@ def main():
     # initializer = box.store, 1
 
     # initializer function for proxy
+    print("initializing proxy function")
     box_encoded_initializer_function = encode_function_data()
 
     proxy = TransparentUpgradeableProxy.deploy(
@@ -27,6 +28,7 @@ def main():
         proxy_admin.address,
         box_encoded_initializer_function,
         {"from": account, "gas_limit": 10000000},
+        # publish_source=True,
     )
 
     print(f"Proxy deployed to {proxy}, you can now upgrade to V2")
@@ -35,9 +37,10 @@ def main():
     # proxy_box.increment({"from": account})
 
     # upgrade
+    print("deploying BOX V2")
     box_v2 = BoxV2.deploy({"from": account})
     upgrade_transaction = upgrade(
-        account, proxy, box_v2.address, proxy_admin_contract=proxy_admin
+        account, proxy, box_v2, proxy_admin_contract=proxy_admin
     )
     print("proxy has been upgraded")
     proxy_box = Contract.from_abi("BoxV2", proxy.address, BoxV2.abi)
